05c0b28365844fdc6f63d104b90e818755038ed6
johnnytran17 <48875871+johnnytran17@users.noreply.github.com>
2021-04-21 23:31:51 +1000

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -100,6 +100,8 @@ const wave1 = "M0 108.306L50 114.323C100 120.34 200 132.374 300 168.476C400 204.
     wave3 = "M0 250L50 238.095C100 226.19 200 202.381 300 166.667C400 130.952 500 83.3333 600 101.19C700 119.048 800 202.381 900 214.286C1000 226.19 1100 166.667 1150 136.905L1200 107.143V0H1150C1100 0 1000 0 900 0C800 0 700 0 600 0C500 0 400 0 300 0C200 0 100 0 50 0H0V250Z",
     wave4 = "M0 125L50 111.111C100 97.2222 200 69.4444 300 97.2222C400 125 500 208.333 600 236.111C700 263.889 800 236.111 900 229.167C1000 222.222 1100 236.111 1150 243.056L1200 250V0H1150C1100 0 1000 0 900 0C800 0 700 0 600 0C500 0 400 0 300 0C200 0 100 0 50 0H0V125Z";
 
+if ($(window).width() > 960) {
+
 var badgeDesign = anime({
     targets: '.badge',
     translateY: 40,
@@ -136,49 +138,8 @@ anime({
     direction: 'alternate',
     easing: 'easeInOutSine'
 });
-anime({
-    targets: ['.wave-top > path', '.wave-bottom > path'],
-    easing: 'linear',
-    duration: 7500,
-    loop: true,
-    d: [
-        {
-            value: [wave1, wave2]
-        },
-        {
-            value: wave3
-        },
-        {
-            value: wave4
-        },
-        {
-            value: wave1
-        },
-  ],
-});
 
-//Make page go to top on refresh
-//$(window).on('onload', function () {
-//    
-//    $(window).scrollTop(0);
-//    window.scrollTo(0, 0);
-//    document.body.style.overflow = 'hidden';
-//});
-
-//document.body.style.overflow = 'hidden';
-//
-////disable scrolling for 3 seconds
-if ($(window).scrollTop() == 0) {
-    document.body.style.overflow = 'hidden';
-    setTimeout(function () {
-        document.body.style.overflow = 'auto';
-    }, 3000);
-}
-
-
-
-
-var fired = false;
+    var fired = false;
 //Change class based on scroll
 $(function () {
     //caches a jQuery object containing the header element
@@ -235,6 +196,38 @@ $(function () {
         });
     });
 });
+}
+
+anime({
+    targets: ['.wave-top > path', '.wave-bottom > path'],
+    easing: 'linear',
+    duration: 7500,
+    loop: true,
+    d: [
+        {
+            value: [wave1, wave2]
+        },
+        {
+            value: wave3
+        },
+        {
+            value: wave4
+        },
+        {
+            value: wave1
+        },
+  ],
+});
+
+////disable scrolling for 3 seconds
+if ($(window).scrollTop() == 0) {
+    document.body.style.overflow = 'hidden';
+    setTimeout(function () {
+        document.body.style.overflow = 'auto';
+    }, 3000);
+}
+
+
 
 
 
